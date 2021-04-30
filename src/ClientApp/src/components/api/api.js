84f7670df9b1c44338d3a00da0409fa132362dda
Alex <aleksandr.kz2000@mail.ru>
2021-04-30 21:36:48 +0500

    fix 3.0 Task 4

--- src/ClientApp/src/components/api/api.js
+++ src/ClientApp/src/components/api/api.js
@@ -1,4 +1,4 @@
 export const restartGameRequest = async () => {
-    const responce = await fetch('https://localhost:5001/api/action');
+    const responce = await fetch('https://localhost:5001/api/restart');
     return responce;
 }
