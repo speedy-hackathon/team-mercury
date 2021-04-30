1eec823549f8a78476a864c5844593a190fdc237
Natalia <natasha.rogachyova@mail.ru>
2021-05-01 00:29:56 +0500

    Add infection radius

--- src/ClientApp/src/components/Person/index.jsx
+++ src/ClientApp/src/components/Person/index.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import styles from "./styles.module.css";
 import {MAX_HEIGHT, MAX_WIDTH} from "../../consts/sizes";
 
-export default function Person({person, onClick}) {
+export default function Person({person, onClick, withInfectionRadius}) {
     const x = (person.position.x / MAX_WIDTH) * 100;
     const y = (person.position.y / MAX_HEIGHT) * 100;
+    const isIll = person.healthStatus === 'Ill';
     return (
-        <div
-            className={`${styles.root} ${styles[person.healthStatus.toLowerCase()]}`}
-            style={{left: `${x}%`, top: `${y}%`}}
-            onClick={() => onClick(person.id)}
-        />
+      <div className={styles.root} style={{left: `${x}%`, top: `${y}%`}}>
+        <div className={withInfectionRadius && isIll ? styles.infectionRadius : ""} />
+        <div className={`${styles.person} ${styles[person.healthStatus.toLowerCase()]}`}
+        onClick={() => onClick(person.id)} />
+      </div>
     );
 }
 
