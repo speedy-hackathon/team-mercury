2bf6f4327e77d40a077612b430b92b1dd27c8ba8
NataashaBanan <49069370+NataashaBanan@users.noreply.github.com>
2021-04-30 20:03:16 +0500

    fix comment

--- src/Services/CityMap.cs
+++ src/Services/CityMap.cs
@@ -9,7 +9,7 @@ namespace covidSim.Services
 
         private const int ComfortablePeopleAmountInHouse = 5;
         private const int HousesInGroup = 8;
-        private const int GroupsInRow = 2; // |..|..|..|..|  |..|..|..|..|  |..|..|..|..|  |..|..|..|..|
+        private const int GroupsInRow = 2; // |..|..|..|..|..|..|..|..|  |..|..|..|..|..|..|..|..|
         
         public const int HouseAmount = Game.PeopleCount / ComfortablePeopleAmountInHouse;
 
