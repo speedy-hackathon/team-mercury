5865ff6f7d7f8b0ff47d54e4354e29d5becb539a
Alex <aleksandr.kz2000@mail.ru>
2021-04-30 22:22:56 +0500

    Task 5 mercury-1

--- src/Services/Person.cs
+++ src/Services/Person.cs
@@ -8,6 +8,8 @@ namespace covidSim.Services
         private const int MaxDistancePerTurn = 30;
         private static Random random = new Random();
         private PersonState state = PersonState.AtHome;
+        private int stateDuration = 0;
+        public bool IsBored => state == PersonState.AtHome && stateDuration >= 5;
 
         public Person(int id, int homeId, CityMap map, PersonHealthStatus healthStatus)
         {
@@ -44,6 +46,7 @@ namespace covidSim.Services
                     break;
             }
         }
+        
 
         private void CalcNextStepForPersonAtHome()
         {
@@ -53,9 +56,11 @@ namespace covidSim.Services
                 var x = random.Next(houseCoordinates.LeftTopCorner.X, houseCoordinates.LeftTopCorner.X + HouseCoordinates.Height);
                 var y = random.Next(houseCoordinates.LeftTopCorner.Y, houseCoordinates.LeftTopCorner.Y + HouseCoordinates.Width);
                 Position = new Vec(x, y);
+                stateDuration++;
                 return;
             }
 
+            stateDuration = 0;
             state = PersonState.Walking;
             CalcNextPositionForWalkingPerson();
         }
@@ -67,7 +72,8 @@ namespace covidSim.Services
             var direction = ChooseDirection();
             var delta = new Vec(xLength * direction.X, yLength * direction.Y);
             var nextPosition = new Vec(Position.X + delta.X, Position.Y + delta.Y);
-
+            stateDuration++;
+            
             if (isCoordInField(nextPosition))
             {
                 Position = nextPosition;
@@ -94,9 +100,11 @@ namespace covidSim.Services
             {
                 Position = homeCenter;
                 state = PersonState.AtHome;
+                stateDuration = 0;
                 return;
             }
 
+            stateDuration++;
             var direction = new Vec(Math.Sign(xDiff), Math.Sign(yDiff));
 
             var xLength = Math.Min(xDistance, MaxDistancePerTurn); 
