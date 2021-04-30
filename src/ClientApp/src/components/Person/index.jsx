e3aec8d954ec7010d52251ac8c2bf9b2ea40cce2
Sergey Uvarov <uvsean@gmail.com>
2021-04-30 20:32:42 +0500

    Task-1. Frontend styles fixes again

--- src/ClientApp/src/components/Person/index.jsx
+++ src/ClientApp/src/components/Person/index.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import styles from "./styles.module.css";
 import {MAX_HEIGHT, MAX_WIDTH} from "../../consts/sizes";
 
-export default function Person({ person, onClick }) {
-  const x = (person.position.x / MAX_WIDTH) * 100;
-  const y = (person.position.y / MAX_HEIGHT) * 100;
-  return (
-    <div
-      className={`${styles.root} ${person.healthStatus === "Ill" ? styles.ill : styles.healthy}`}
-      style={{ left: `${x}%`, top: `${y}%` }}
-      onClick={() => onClick(person.id)}
-    />
-  );
+export default function Person({person, onClick}) {
+    const x = (person.position.x / MAX_WIDTH) * 100;
+    const y = (person.position.y / MAX_HEIGHT) * 100;
+    return (
+        <div
+            className={`${styles.root} ${styles[person.healthStatus.toLowerCase()]}`}
+            style={{left: `${x}%`, top: `${y}%`}}
+            onClick={() => onClick(person.id)}
+        />
+    );
 }
+
